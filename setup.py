2f230075d3ee9c1e266b0a197ca42a523d74d9c3
Michael Strain <strainmike@gmail.com>
2022-03-22 10:00:46 -0400

    Adding a setup.py so a pylabview package can be built

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+def get_long_description():
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    if not os.path.exists("README.md"):
+        return ""
+    else:
+        with open(os.path.join(this_dir, "README.md")) as readme:
+            return readme.read().strip()
+
+setup(name="pylabview",
+    description="Python LabVIEW File Type Parser",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    version="0.1",
+    author="mefistotelis",
+    install_requires=["Pillow"],
+    url="https://github.com/mefistotelis/pylabview",
+    license="MIT",
+    packages=find_packages(),
+    package_data={'pylabview':['assets/tom-thumb.pbm',
+                              'assets/tom-thumb.pil',
+                              'assets/tom-thumb.txt']},
+    entry_points={'console_scripts':['readRSRC = pylabview.readRSRC:main',
+                                     'modRSRC = pylabview.modRSRC:main']},
+)
