145585b3a83bc35a3a545f5f1686b7eeb2889ae8
mefistotelis <mefistotelis@gmail.com>
2022-05-08 10:50:46 +0200

    setup: Provided more info about the package
    Provided metadata regarding keywords, classifiers and bug reporting.
    Also, checked required python version using netromdk/vermin, and
    added requirement of thar version.

--- setup.py
+++ setup.py
@@ -9,19 +9,45 @@ def get_long_description():
         with open(os.path.join(this_dir, "README.md")) as readme:
             return readme.read().strip()
 
-setup(name="pylabview",
+setup(
+    name="pylabview",
+    version="0.1.1",
+
     description="Python LabVIEW File Type Parser",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
-    version="0.1.1",
+
     author="mefistotelis",
+
     install_requires=["Pillow"],
+    python_requires=">=3.4",
+
     url="https://github.com/mefistotelis/pylabview",
     license="MIT",
+
+    classifiers=[
+      "Intended Audience :: Developers",
+
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 3",
+
+      "License :: OSI Approved :: MIT License",
+
+      "Topic :: Utilities",
+      "Topic :: Software Development",
+    ],
+
+    keywords="labview vi instruments parser extractor reverse-engineering development",
+
     packages=find_packages(),
     package_data={'pylabview':['assets/tom-thumb.pbm',
                               'assets/tom-thumb.pil',
                               'assets/tom-thumb.txt']},
     entry_points={'console_scripts':['readRSRC = pylabview.readRSRC:main',
                                      'modRSRC = pylabview.modRSRC:main']},
+
+    project_urls={
+      "Bug Reports": "https://github.com/mefistotelis/pylabview/issues",
+      "Source": "https://github.com/mefistotelis/pylabview/",
+    },
 )
